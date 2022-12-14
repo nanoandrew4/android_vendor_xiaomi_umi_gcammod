d19cda3b07059743b96dbadf3b837ecaf2215507
nanoandrew4 <nanoandrew4@gmail.com>
2022-12-14 10:14:12 +0100

    Add optional libs used by apk

--- Android.mk
+++ Android.mk
@@ -7,4 +7,17 @@ LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_PRODUCT_MODULE := true
 LOCAL_OVERRIDES_PACKAGES := Aperture Camera2
+LOCAL_OPTIONAL_USES_LIBRARIES := com.google.android.gestureservice \
+	com.google.android.camera2 \
+	com.google.android.camera.experimental2015 \
+	com.google.android.camera.experimental2016 \
+	com.google.android.camera.experimental2017 \
+	com.google.android.camera.experimental2018 \
+	com.google.android.camera.experimental2019 \
+	com.google.android.camera.experimental2020 \
+	com.google.android.camera.experimental2020_midyear \
+	com.google.android.camera.experimental2021 \
+	androidx.window.extensions \
+	androidx.window.sidecar \
+	com.google.android.wearable
 include $(BUILD_PREBUILT)
